Fixed sign on actor loss, minor refactoring in model
still looking for hyperparams that work!!

neural_combinatorial_rl.py
@@ -12,32 +12,36 @@ from beam_search import Beam
 class Encoder(nn.Module):
     """Maps a graph represented as an input sequence
     to a hidden vector"""
-    def __init__(self, input_dim, hidden_dim, n_layers, dropout, use_cuda):
+    def __init__(self, input_dim, hidden_dim, use_cuda):
         super(Encoder, self).__init__()
         self.hidden_dim = hidden_dim
-        self.n_layers = n_layers
-        self.lstm = nn.LSTM(input_dim, hidden_dim, n_layers,
-                dropout=dropout)
+        self.lstm = nn.LSTM(input_dim, hidden_dim)
         self.use_cuda = use_cuda
-    
+        self.enc_init_state = self.init_hidden(hidden_dim)
+
     def forward(self, x, hidden):
         output, hidden = self.lstm(x, hidden)
         return output, hidden
     
-    def init_hidden(self, inputs):
-        batch_size = inputs.size(1)
-        hx = Variable(torch.zeros(self.n_layers,
-            batch_size,
-            self.hidden_dim),
-            requires_grad=False)
-        cx = Variable(torch.zeros(self.n_layers,
-            batch_size,
-            self.hidden_dim),
-            requires_grad=False)
+    def init_hidden(self, hidden_dim):
+        """Trainable initial hidden state"""
+        enc_init_hx = torch.FloatTensor(hidden_dim)
         if self.use_cuda:
-            return hx.cuda(), cx.cuda()
-        else:
-            return hx, cx
+            enc_init_hx = enc_init_hx.cuda()
+
+        enc_init_hx = nn.Parameter(enc_init_hx)
+        enc_init_hx.data.uniform_(-(1. / math.sqrt(hidden_dim)),
+                1. / math.sqrt(hidden_dim))
+
+        enc_init_cx = torch.FloatTensor(hidden_dim)
+        if self.use_cuda:
+            enc_init_cx = enc_init_cx.cuda()
+
+        enc_init_cx = nn.Parameter(enc_init_cx)
+        enc_init_cx.data.uniform_(-(1. / math.sqrt(hidden_dim)),
+                1. / math.sqrt(hidden_dim))
+        return (enc_init_hx, enc_init_cx)
+
 
 class Attention(nn.Module):
     """A generic attention module for a decoder in seq2seq"""
@@ -91,7 +95,6 @@ class Decoder(nn.Module):
             terminating_symbol,
             use_tanh,
             decode_type,
-            dropout,
             n_glimpses=1,
             beam_size=0,
             use_cuda=True):
@@ -106,19 +109,13 @@ class Decoder(nn.Module):
         self.beam_size = beam_size
         self.use_cuda = use_cuda
 
-        self.dropout = nn.Dropout(p=dropout)
         self.input_weights = nn.Linear(embedding_dim, 4 * hidden_dim)
         self.hidden_weights = nn.Linear(hidden_dim, 4 * hidden_dim)
-        self.linear_hidden_out = nn.Linear(hidden_dim * 2, hidden_dim)
 
         self.pointer = Attention(hidden_dim, use_tanh=use_tanh, C=tanh_exploration, use_cuda=self.use_cuda)
-        self.glimpse = Attention(hidden_dim, use_tanh=use_tanh, C=tanh_exploration, use_cuda=self.use_cuda)
+        self.glimpse = Attention(hidden_dim, use_tanh=False, use_cuda=self.use_cuda)
         self.sm = nn.Softmax()
 
-        # self.neg_inf = Variable(torch.Tensor([float('-inf')]))
-        # if self.use_cuda:
-        #     self.neg_inf = self.neg_inf.cuda()
-        
     def apply_mask_to_logits(self, step, logits, mask, prev_idxs):    
         if mask is None:
             mask = torch.zeros(logits.size()).byte()
@@ -128,8 +125,6 @@ class Decoder(nn.Module):
         # to prevent them from being reselected. 
         # Or, allow re-selection and penalize in the objective function
         if prev_idxs is not None:
-            #mask_size = logits.size(0) * step
-            #n_inf = self.neg_inf.repeat(mask_size)
             # set most recently selected idx values to 1
             mask[[x for x in range(logits.size(0))],
                     prev_idxs.data] = 1
@@ -141,15 +136,13 @@ class Decoder(nn.Module):
         Args:
             decoder_input: The initial input to the decoder
                 size is [batch_size x embedding_dim]. Trainable parameter.
-            embedded_inputs: [sourceL x batch_size x embeddign_dim]
+            embedded_inputs: [sourceL x batch_size x embedding_dim]
             hidden: the prev hidden state, size is [batch_size x hidden_dim]. 
                 Initially this is set to (enc_h[-1], enc_c[-1])
             context: encoder outputs, [sourceL x batch_size x hidden_dim] 
         """
         def recurrence(x, hidden, logit_mask, prev_idxs, step):
             
-            x = self.dropout(x)
-
             hx, cx = hidden  # batch_size x hidden_dim
             
             gates = self.input_weights(x) + self.hidden_weights(hx)
@@ -171,7 +164,7 @@ class Decoder(nn.Module):
                 # [batch_size x h_dim x 1]
                 g_l = torch.bmm(ref, self.sm(logits).unsqueeze(2)).squeeze(2) 
             _, logits = self.pointer(g_l, context)
-            #h_tilde = F.tanh(self.linear_hidden_out(torch.cat((h_tilde, hy), 1)))
+            
             logits, logit_mask = self.apply_mask_to_logits(step, logits, logit_mask, prev_idxs)
             probs = self.sm(logits)
             return hy, cy, probs, logit_mask
@@ -186,7 +179,6 @@ class Decoder(nn.Module):
        
         if self.decode_type == "stochastic":
             for i in steps:
-
                 hx, cx, probs, mask = recurrence(decoder_input, hidden, mask, idxs, i)
                 hidden = (hx, cx)
                 # select the next inputs for the decoder [batch_size x hidden_dim]
@@ -316,7 +308,6 @@ class PointerNetwork(nn.Module):
     """The pointer network, which is the core seq2seq 
     model"""
     def __init__(self, 
-            encoder,
             embedding_dim,
             hidden_dim,
             max_decoding_len,
@@ -324,12 +315,14 @@ class PointerNetwork(nn.Module):
             n_glimpses,
             tanh_exploration,
             use_tanh,
-            dropout,
             beam_size,
             use_cuda):
         super(PointerNetwork, self).__init__()
 
-        self.encoder = encoder
+        self.encoder = Encoder(
+                embedding_dim,
+                hidden_dim,
+                use_cuda)
 
         self.decoder = Decoder(
                 embedding_dim,
@@ -339,11 +332,11 @@ class PointerNetwork(nn.Module):
                 use_tanh=use_tanh,
                 terminating_symbol=terminating_symbol,
                 decode_type="stochastic",
-                dropout=dropout,
                 n_glimpses=n_glimpses,
                 beam_size=beam_size,
                 use_cuda=use_cuda)
-        
+
+        # Trainable initial hidden states
         dec_in_0 = torch.FloatTensor(embedding_dim)
         if use_cuda:
             dec_in_0 = dec_in_0.cuda()
@@ -358,11 +351,13 @@ class PointerNetwork(nn.Module):
             inputs: [sourceL x batch_size x embedding_dim]
         """
         
-        encoder_h0, encoder_c0 = self.encoder.init_hidden(inputs)        
-
-        # encoder forward pass
-        enc_outputs, (enc_h_t, enc_c_t) = self.encoder(inputs, (encoder_h0, encoder_c0))
+        (encoder_hx, encoder_cx) = self.encoder.enc_init_state
+        encoder_hx = encoder_hx.unsqueeze(0).repeat(inputs.size(1), 1).unsqueeze(0)       
+        encoder_cx = encoder_cx.unsqueeze(0).repeat(inputs.size(1), 1).unsqueeze(0)       
         
+        # encoder forward pass
+        enc_h, (enc_h_t, enc_c_t) = self.encoder(inputs, (encoder_hx, encoder_cx))
+
         dec_init_state = (enc_h_t[-1], enc_c_t[-1])
     
         # repeat decoder_in_0 across batch
@@ -371,7 +366,7 @@ class PointerNetwork(nn.Module):
         (pointer_probs, input_idxs), dec_hidden_t = self.decoder(decoder_input,
                 inputs,
                 dec_init_state,
-                enc_outputs)
+                enc_h)
 
         return pointer_probs, input_idxs
         
@@ -379,7 +374,7 @@ class PointerNetwork(nn.Module):
 class CriticNetwork(nn.Module):
     """Useful as a baseline in REINFORCE updates"""
     def __init__(self,
-            encoder,
+            embedding_dim,
             hidden_dim,
             n_process_block_iters,
             tanh_exploration,
@@ -390,8 +385,13 @@ class CriticNetwork(nn.Module):
         self.hidden_dim = hidden_dim
         self.n_process_block_iters = n_process_block_iters
 
-        self.encoder = encoder
-        self.process_block = Attention(hidden_dim, use_tanh=use_tanh, C=tanh_exploration, use_cuda=use_cuda)
+        self.encoder = Encoder(
+                embedding_dim,
+                hidden_dim,
+                use_cuda)
+        
+        self.process_block = Attention(hidden_dim,
+                use_tanh=use_tanh, C=tanh_exploration, use_cuda=use_cuda)
         self.sm = nn.Softmax()
         self.decoder = nn.Sequential(
                 nn.Linear(hidden_dim, hidden_dim),
@@ -404,11 +404,14 @@ class CriticNetwork(nn.Module):
         Args:
             inputs: [embedding_dim x batch_size x sourceL] of embedded inputs
         """
-        encoder_h0, encoder_c0 = self.encoder.init_hidden(inputs)        
+         
+        (encoder_hx, encoder_cx) = self.encoder.enc_init_state
+        encoder_hx = encoder_hx.unsqueeze(0).repeat(inputs.size(1), 1).unsqueeze(0)
+        encoder_cx = encoder_cx.unsqueeze(0).repeat(inputs.size(1), 1).unsqueeze(0)       
         
         # encoder forward pass
-        enc_outputs, (enc_h_t, enc_c_t) = self.encoder(inputs, (encoder_h0, encoder_c0))
-       
+        enc_outputs, (enc_h_t, enc_c_t) = self.encoder(inputs, (encoder_hx, encoder_cx))
+        
         # grab the hidden state and process it via the process block 
         process_block_state = enc_h_t[-1]
         for i in range(self.n_process_block_iters):
@@ -432,10 +435,8 @@ class NeuralCombOptRL(nn.Module):
             terminating_symbol,
             n_glimpses,
             n_process_block_iters,
-            n_layers,
             tanh_exploration,
             use_tanh,
-            dropout,
             beam_size,
             objective_fn,
             is_train,
@@ -446,15 +447,8 @@ class NeuralCombOptRL(nn.Module):
         self.is_train = is_train
         self.use_cuda = use_cuda
 
-        self.encoder = Encoder(
-                embedding_dim,
-                hidden_dim,
-                n_layers,
-                dropout,
-                use_cuda)
         
         self.actor_net = PointerNetwork(
-                self.encoder,
                 embedding_dim,
                 hidden_dim,
                 max_decoding_len,
@@ -462,25 +456,22 @@ class NeuralCombOptRL(nn.Module):
                 n_glimpses,
                 tanh_exploration,
                 use_tanh,
-                dropout,
                 beam_size,
                 use_cuda)
+        
         self.critic_net = CriticNetwork(
-                self.encoder,
+                embedding_dim,
                 hidden_dim,
                 n_process_block_iters,
                 tanh_exploration,
-                use_tanh,
+                False,
                 use_cuda)
        
         embedding_ = torch.FloatTensor(input_dim,
             embedding_dim)
-        
         if self.use_cuda: 
             embedding_ = embedding_.cuda()
-
-        self.embedding = nn.Parameter(embedding_)
-         
+        self.embedding = nn.Parameter(embedding_) 
         self.embedding.data.uniform_(-(1. / math.sqrt(embedding_dim)),
             1. / math.sqrt(embedding_dim))
         

trainer.py
@@ -11,6 +11,7 @@ import torch
 print(torch.__version__)
 import torch.optim as optim
 import torch.autograd as autograd
+from torch.optim import lr_scheduler
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from tensorboard_logger import configure, log_value
@@ -33,14 +34,13 @@ parser.add_argument('--val_size', default=10000, help='')
 parser.add_argument('--embedding_dim', default=128, help='Dimension of input embedding')
 parser.add_argument('--hidden_dim', default=128, help='Dimension of hidden layers in Enc/Dec')
 parser.add_argument('--n_process_blocks', default=3, help='Number of process block iters to run in the Critic network')
-parser.add_argument('--n_layers', default=1, help='Number of LSTM layers in the encoder')
 parser.add_argument('--n_glimpses', default=2, help='No. of glimpses to use in the pointer network')
 parser.add_argument('--use_tanh', type=str2bool, default=True)
-parser.add_argument('--tanh_exploration', default=20, help='Hyperparam controlling exploration in the pointer net by scaling the tanh in the softmax')
+parser.add_argument('--tanh_exploration', default=10, help='Hyperparam controlling exploration in the pointer net by scaling the tanh in the softmax')
 parser.add_argument('--dropout', default=0., help='')
 parser.add_argument('--terminating_symbol', default='<0>', help='')
 parser.add_argument('--beam_size', default=1, help='Beam width for beam search')
-parser.add_argument('--entropy_coeff', default=0.01, type=float, help='hyperparam for controlling entropy penalty')
+
 # Training
 parser.add_argument('--actor_net_lr', default=1e-4, help="Set the learning rate for the actor network")
 parser.add_argument('--critic_net_lr', default=1e-4, help="Set the learning rate for the critic network")
@@ -113,6 +113,8 @@ else:
 
 # Load the model parameters from a saved state
 if args['load_path'] != '':
+    print('  [*] Loading model from {}'.format(args['load_path']))
+
     model = torch.load(
         os.path.join(
             os.getcwd(),
@@ -130,10 +132,8 @@ else:
         args['terminating_symbol'],
         int(args['n_glimpses']),
         int(args['n_process_blocks']), 
-        int(args['n_layers']), 
         float(args['tanh_exploration']),
         args['use_tanh'],
-        float(args['dropout']),
         int(args['beam_size']),
         reward_fn,
         args['is_train'],
@@ -154,10 +154,18 @@ critic_mse = torch.nn.MSELoss()
 critic_optim = optim.Adam(model.critic_net.parameters(), lr=float(args['critic_net_lr']))
 actor_optim = optim.Adam(model.actor_net.parameters(), lr=float(args['actor_net_lr']))
 
-training_dataloader = DataLoader(training_dataset, batch_size=int(args['batch_size']),
-    shuffle=True, num_workers=4)
+actor_scheduler = lr_scheduler.MultiStepLR(actor_optim,
+        range(int(args['actor_lr_decay_step']), int(args['actor_lr_decay_step']) * 1000,
+            int(args['actor_lr_decay_step'])), gamma=float(args['actor_lr_decay_rate']))
 
-validation_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=True, num_workers=4)
+critic_scheduler = lr_scheduler.MultiStepLR(critic_optim,
+        range(int(args['critic_lr_decay_step']), int(args['critic_lr_decay_step']) * 1000,
+            int(args['critic_lr_decay_step'])), gamma=float(args['critic_lr_decay_rate']))
+
+training_dataloader = DataLoader(training_dataset, batch_size=int(args['batch_size']),
+    shuffle=True, num_workers=1)
+
+validation_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=True, num_workers=1)
 
 
 if args['use_cuda']:
@@ -184,6 +192,7 @@ for i in range(epoch, epoch + int(args['n_epochs'])):
         for batch_id, sample_batch in enumerate(tqdm(training_dataloader,
                 disable=args['disable_progress_bar'])):
 
+
             bat = Variable(sample_batch)
             if args['use_cuda']:
                 bat = bat.cuda()
@@ -208,7 +217,7 @@ for i in range(epoch, epoch + int(args['n_epochs'])):
 
             # multiply each time step by the advanrate
             reinforce = advantage * logprobs
-            actor_loss = -args['reward_scale'] * reinforce.mean()
+            actor_loss = args['reward_scale'] * reinforce.mean()
             actor_optim.zero_grad()
             
             actor_loss.backward(retain_graph=True)
@@ -218,7 +227,9 @@ for i in range(epoch, epoch + int(args['n_epochs'])):
                     float(args['max_grad_norm']), norm_type=2)
 
             actor_optim.step()
-           
+            actor_scheduler.step()
+            critic_scheduler.step()
+
             R = R.detach()
             critic_loss = critic_mse(v.squeeze(1), R)
             critic_optim.zero_grad()
@@ -251,19 +262,18 @@ for i in range(epoch, epoch + int(args['n_epochs'])):
                 #print('Example train input: {}'.format(example_input))
                 print('Example train output: {}'.format(example_output))
 
-            if step % int(args['actor_lr_decay_step']) == 0 and \
-                    float(args['actor_net_lr']) > 1e-10:
+            """
+            if step % int(args['actor_lr_decay_step']) == 0:
                 args['actor_net_lr'] = float(args['actor_net_lr']) \
                         * float(args['actor_lr_decay_rate'])
                 actor_optim = optim.Adam(model.actor_net.parameters(),
                         lr=float(args['actor_net_lr']))
-            if step % int(args['critic_lr_decay_step']) == 0 and \
-                    float(args['critic_net_lr']) > 1e-10:
+            if step % int(args['critic_lr_decay_step']) == 0:
                 args['critic_net_lr'] = float(args['critic_net_lr']) \
                         * float(args['critic_lr_decay_rate'])
                 critic_optim = optim.Adam(model.critic_net.parameters(),
                         lr=float(args['critic_net_lr']))
-
+            """
     # Use beam search decoding for validation
     model.actor_net.decoder.decode_type = "beam_search"
     
@@ -325,4 +335,4 @@ for i in range(epoch, epoch + int(args['n_epochs'])):
             training_dataset = tsp_task.TSPDataset(train=True, size=size,
                 num_samples=int(args['train_size']))
             training_dataloader = DataLoader(training_dataset, batch_size=int(args['batch_size']),
-                shuffle=True, num_workers=4)
+                shuffle=True, num_workers=1)

tsp_task.py
@@ -41,9 +41,10 @@ def reward(sample_solution, USE_CUDA=False):
     # min_len = 3.5
     # max_len = 10.
     # TODO: generalize this for any TSP size
-    tour_len = -0.1538*tour_len + 1.538 
-    tour_len[tour_len < 0.] = 0.
+    #tour_len = -0.1538*tour_len + 1.538 
+    #tour_len[tour_len < 0.] = 0.
     return tour_len
+
 
 #######################################
 # Functions for downloading dataset
@@ -213,8 +214,11 @@ def create_dataset(
 #######################################
 class TSPDataset(Dataset):
     
-    def __init__(self, dataset_fname=None, train=False, size=50, num_samples=1000000):
+    def __init__(self, dataset_fname=None, train=False, size=50, num_samples=1000000, random_seed=1111):
         super(TSPDataset, self).__init__()
+        #start = torch.FloatTensor([[-1], [-1]]) 
+        
+        torch.manual_seed(random_seed)
 
         self.data_set = []
         if not train:
@@ -229,6 +233,7 @@ class TSPDataset(Dataset):
             # randomly sample points uniformly from [0, 1]
             for l in tqdm(range(num_samples)):
                 x = torch.FloatTensor(2, size).uniform_(0, 1)
+                #x = torch.cat([start, x], 1)
                 self.data_set.append(x)
 
         self.size = len(self.data_set)